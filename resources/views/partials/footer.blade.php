d422086fecdf58ab7173810c7cc603e1fba8d114
vincentsimonmarufu <vincentsimonmarufu@gmail.com>
2023-05-23 16:05:58 +0200

    updated index page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,79 @@
+    <!-- main-footer -->
+    <footer class="main-footer">
+        <div class="auto-container">
+            <div class="footer-top">
+                <div class="widget-section wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+                    <div class="row clearfix">
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget logo-widget">
+                                <figure class="footer-logo"><a href="index-2.html"><img src="assets/images/LOGO.jpg" alt=""></a></figure>
+                                <div class="text">
+                                    <p>Lorem ipsum dolor amet consectetur adipielit sed eiusm tempor incididunt ut labore dolore.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget links-widget">
+                                <div class="widget-title">
+                                    <h3>Quick Link</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <ul>
+                                        <li><a href="index-2.html">Company History</a></li>
+                                        <li><a href="index-2.html">About Us</a></li>
+                                        <li><a href="index-2.html">Contact Us</a></li>
+                                        <li><a href="index-2.html">Services</a></li>
+                                        <li><a href="index-2.html">Privacy Policy</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget links-widget">
+                                <div class="widget-title">
+                                    <h3>Services</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <ul>
+                                        <li><a href="index-2.html">Data Analysis</a></li>
+                                        <li><a href="index-2.html">Grant Writing</a></li>
+                                        <li><a href="index-2.html">Impact Assessment</a></li>
+                                        <li><a href="index-2.html">Survey Design</a></li>
+                                        <li><a href="index-2.html">Third Party M&E</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                            <div class="footer-widget contact-widget">
+                                <div class="widget-title">
+                                    <h3>Contact Info</h3>
+                                </div>
+                                <div class="widget-content">
+                                    <ul>
+                                        <li>Address: 15 Ridge Road, Avondale, Harare</li>
+                                        <li>Phone: +263 779 970 322 | +263 779777244 | +263 71 900 3123</li>
+                                        <li><a href="mailto:info@c4impacteval.org">Email: info@c4impacteval.org</a></li>
+                                    </ul>
+                                </div>
+                                <ul class="social-links clearfix">
+                                    <li><a href="index-2.html"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="index-2.html"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="index-2.html"><i class="fab fa-linkedin-in"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-bottom clearfix">
+                <div class="copyright pull-left">
+                    <p><a href="index-2.html">CFIERD</a> &copy; {{ date('Y') }} All Right Reserved</p>
+                </div>
+                <ul class="footer-nav pull-right">
+                    <li><a href="index-2.html">Terms of Service</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    <!-- main-footer end -->
